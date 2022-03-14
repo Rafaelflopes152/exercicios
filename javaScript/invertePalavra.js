b202d0b71e3e8d0e78346c1727258075fe7611a1
Rafael de Figueiredo Lopes, T21-B-SH-55 <rafaelflopes18@gmail.com>
2022-03-14 18:03:41 -0300

    DOM

--- javaScript/invertePalavra.js
+++ javaScript/invertePalavra.js
@@ -2,9 +2,10 @@ let word = 'tryber';
 let inWord;
 let cont = word.length;
 for (let i=0;i<word.length;i++){
-    inWord=Word[cont];
+    // inWord=Word[cont];
+    console.log(word[cont]);
     cont =cont - 1;
-    console.log(inWord);
+    // console.log(Word);
     // console.log(cont);
 }
 // console.log("Apalavra é: "+word+" e invertida fica: "+inWord);
