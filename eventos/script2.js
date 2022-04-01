496791746b54e58c0c7a7dac5609ea8cc9498d90
Rafael de Figueiredo Lopes, T21-B-SH-55 <rafaelflopes18@gmail.com>
2022-04-01 18:33:26 -0300

    exercicios 7.1

---
+++ eventos/script2.js
@@ -0,0 +1,107 @@
+// variaveis do sistema
+const criarTarefa = document.querySelector("#criar-tarefa");
+// Obtendo a referência do elemento body
+const body = document.getElementsByTagName("body")[0];
+// Obtendo a referência do elemento 'lista-tarefas'
+const listaTarefa = document.getElementById("lista-tarefas");
+let lis = '';
+let textoTarefa = '';
+
+// muda cor de fundo quando clicado
+listaTarefa.addEventListener('click', function (event) {
+    // console.log(event.target);
+    limpaSelecao(lis.length);
+    // event.target.setAttribute("class", 'selected');
+    event.target.style.backgroundColor = 'gray';
+});
+
+listaTarefa.addEventListener('dblclick', function (event) {
+    let cName = event.target.className;
+    // console.log(cName);
+    if (cName === 'completed') {
+        event.target.setAttribute("class", ' ');
+    } else {
+        event.target.setAttribute("class", 'completed');
+    }
+});
+
+//limpa seleção
+function limpaSelecao(lista) {
+    lis = document.getElementsByTagName('li');
+
+    for (let i = 0; i < lista; i += 1) {
+
+        lis[i].style.backgroundColor = 'white';
+    }
+}
+
+
+// espera o click do botão
+criarTarefa.onclick = function () {
+    textoTarefa = document.querySelector("#texto-tarefa");
+    inseriTarefa(textoTarefa.value);
+    textoTarefa.value = '';
+}
+
+// inseri tarefa
+function inseriTarefa(texto) {
+    let linha = document.createElement("li");
+    let tarefa = document.createTextNode(texto);
+    linha.appendChild(tarefa);
+    listaTarefa.appendChild(linha);
+    body.appendChild(listaTarefa);
+}
+// function genera_tabela(numero) {
+//     let N = numero;
+//     console.log(N);
+//     // Obtendo a referência do elemento body
+//     // var body = document.getElementsByTagName("body")[0];
+//     // Obtendo a referência do elemento main
+//     var main = document.getElementsByTagName("main")[0];
+
+//     // Crie um elemento <table> e um elemento <tbody>
+//     var tabela = document.createElement("table");
+//     var tblBody = document.createElement("tbody");
+
+//     // CRIE as células
+//     for (var i = 0; i < N; i++) {
+//         // Criar as linhas da tabela
+//         var fila = document.createElement("tr");
+
+//         for (var j = 0; j < N; j++) {
+//             // Crie um elemento <td> e um nó de texto, faça do nó de texto o conteúdo do <td>, coloque o elemento <td> no final da linha da tabela
+//             var celula = document.createElement("td");
+//             var textoCelda = document.createTextNode(i + " " + j);
+//             celula.appendChild(textoCelda);
+//             fila.appendChild(celula);
+//             celula.setAttribute("class", "pixel");
+//             // const celula2 = document.getElementsByTagName('td');
+//             // if (celula2.classList) celula2.classList.add("pixel");
+//             // else celula2.className += " pixel";
+//         }
+
+//         // adicione a linha ao final da tabela (no final do elemento tblbody)
+//         tblBody.appendChild(fila);
+//     }
+
+//     // posicione o <tbody> abaixo do elemento <table>
+//     tabela.appendChild(tblBody);
+//     // acrescenta <table> em <main>
+//     main.appendChild(tabela);
+//     // acrescenta <main> em <body>
+//     body.appendChild(main);
+//     // modifica o atributo "border" da tabela e o define como "2";
+//     tabela.setAttribute("border", "2");
+//     tabela.setAttribute("id", 'pixel-board');
+// }
+
+// monitora a tabela e pinta a celula quando clicado
+// function monitoraClick(){
+//     let lis = document.getElementsByTagName('li');
+// for (let i = 0; i < lis.length; i += 1) {
+//     lis[i].addEventListener('click', function (e) {
+//       // 'this' no site https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/this
+//       lis.style.backgroundColor = 'gray';
+//     });
+//   }
+// }
--- varios_exemplos/scriptArt.js
+++ varios_exemplos/scriptArt.js
@@ -9,30 +9,79 @@ const body = document.getElementsByTagName("body")[0];
 const main = document.getElementsByTagName("main")[0];
 // Obtendo a referência do elemento 'pixel-board'
 const pixelBoard = document.getElementById("pixel-board");
-let selected = '';
-let cores = 4;
-let board = 5;
+// board
+const pixel = document.getElementsByClassName('pixel');
+// botão limpa tudo
+const clear = document.getElementById('clear-board');
 
+let selected = '';
+let cores = 'rgb(0,0,0)';
+let board = 6;
+
+genera_tabela(board);
+
+// recebe o tamanho do board e inicia a função que gera a tabela.
+let boardSize = '';
+const gBoard = document.querySelector("#generate-board");
+gBoard.onclick = function () {
+    boardSize = document.querySelector("#board-size");
+    // // verifica tamanho do quadro de pixels se esta entre 5 e 50
+    // if (boardSize.value < 5) {
+    //     // mostre um alert caso menor que 5
+    //     alert('Board inválido!');
+    //     genera_tabela(5);
+    // } else if (boardSize.value > 50) {
+    //     // mostre um alert caso maior que 50
+    //     alert('Board inválido!');
+    //     genera_tabela(50);
+    // } else {
+    //     genera_tabela(boardSize.value);
+    // }
+    inserirLinhaTabela(boardSize);
+
+}
 colorPalete.addEventListener('click', function (event) {
-    // console.log(event.target);
     limpaSelecao();
     event.target.setAttribute("class", 'color selected');
-    //event.target.style.backgroundColor = 'gray';
+    corSelecionada(event.target.id);
 });
+
+pixelBoard.addEventListener('click', function (event) {
+    event.target.style.backgroundColor = cores;
+});
+
+// pinta todas as pexil de branco
+clear.addEventListener('click', function (e) {
+    // console.log(pixel.length);
+    for (let i = 0; i < pixel.length; i += 1) {
+        pixel[i].style.backgroundColor = 'white';
+    }
+});
+
+// seleciona cor
+function corSelecionada(ID) {
+    let cor
+    if (ID === "color1") {
+        cor = "rgb(0,0,0)";
+    } else if (ID === "color2") {
+        cor = 'rgb(255, 0, 0)';
+    } else if (ID === "color3") {
+        cor = 'rgb(0, 0, 255)';
+    } else if (ID === "color4") {
+        cor = 'rgb(0, 255, 0)';
+    }
+    cores = cor;
+    return (cores);
+}
 
 //limpa seleção
 function limpaSelecao() {
     selected = document.getElementsByClassName("selected");
-
     for (let i = 0; i < selected.length; i += 1) {
-        console.log(i);
         // funcao 'replace' conforme site https://www.delftstack.com/pt/howto/javascript/javascript-change-css-class/
         selected[i].classList.replace('selected', 'color');
-
     }
 }
-
-genera_tabela(board);
 
 //gera tabela
 function genera_tabela(numero) {
@@ -66,4 +115,29 @@ function genera_tabela(numero) {
     // modifica o atributo "border" da tabela e o define como "2";
     // tabela.setAttribute("border", "2");
     pixelBoard.setAttribute("id", 'pixel-board');
+
+}
+
+// Função responsável por inserir linhas na tabela
+
+function inserirLinhaTabela(N) {
+    // pixelBoard
+    // Captura a quantidade de linhas já existentes na tabela
+    let numRows = pixelBoard.rows.length;
+    // Captura a quantidade de colunas da última linha da tabela
+    let numCols = pixelBoard.rows[numRows - 1].cells.length;
+    for (let i = 0; i < numCols; i += 1) {
+        // Insere uma linha no fim da tabela.
+        var newCol = pixelBoard.insertRow(N);
+        var newRow = pixelBoard.insertRow(i);
+        console.log(newRow);
+
+        // Faz um loop para criar as colunas
+        for (var j = 0; j < numCols; j++) {
+            // Insere uma coluna na nova linha 
+            newCell = newRow.insertCell(j);
+            newCell.setAttribute("class", "pixel");
+
+        }
+    }
 }
